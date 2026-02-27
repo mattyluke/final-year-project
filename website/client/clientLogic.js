4b061651f810ab95dbcc7278205111ace8b562f0
mattyluke <matthewwheeler801@gmail.com>
2026-02-27 18:13:26 +0000

    Populated website/client repository with client-side website source code

---
+++ website/client/clientLogic.js
@@ -0,0 +1,117 @@
+export function getValidMoves(board, pieceCoord) {
+
+    if(!pieceCoord) return [];
+
+    const [x, y, z] = pieceCoord;
+    const key = `${x},${y},${z}`;
+
+    if (!board[key]) return [];
+    if (board[key].occupation === "N") return [];
+
+    const directions = {
+        "ur" : [1, 0, -1],
+        "mr" : [1, -1, 0],
+        "dr" : [0, -1, 1],
+        "dl" : [-1, 0, 1],
+        "ml" : [-1, 1, 0],
+        "ul" : [0, 1, -1]
+    };
+
+    const validMoves = [];
+
+    for (const dirKey in directions) {
+        const dir = directions[dirKey];
+        let delta = 1;
+        let lastValid = null;
+
+        while(true) {
+            const newX = x + dir[0] * delta;
+            const newY = y + dir[1] * delta;
+            const newZ = z + dir[2] * delta;
+
+            const key = `${newX},${newY},${newZ}`;
+
+            if(!(key in board))break;
+            if(board[key].occupation !== "N")break;
+
+            lastValid = key;
+            delta++;
+        }
+        if (lastValid){
+            validMoves.push(lastValid);
+        }
+    }
+
+    return validMoves;
+}
+
+function neighbourCount(piece, board){
+    const directions = [[1, 0, -1], [1, -1, 0], [0, 1, -1], [0, -1, 1], [-1, 0, 1], [-1, 1, 0]];
+    const [x, y, z] = piece;
+    let count = 0;
+
+    for (const [dx, dy, dz] of directions) {
+        const key = `${x + dx},${y + dy},${z + dz}`;
+        if(key in board){
+            count ++;
+        }
+    }
+
+    return count;
+}
+
+export function getPiecesToMove(board){
+    let canMovePiecesKeys = []
+
+    for (const key in board){
+        if (neighbourCount(board[key].coord, board) < 5 && board[key].occupation === "N"){
+            canMovePiecesKeys.push(key);
+        }
+    }
+    return canMovePiecesKeys
+}
+
+export function getPossibleBoardMoves(board, removedPiece){
+    const directions = [
+        [1, 0, -1], [1, -1, 0], [0, 1, -1], [0, -1, 1], [-1, 0, 1], [-1, 1, 0]
+    ];
+
+    const [x, y, z] = removedPiece;
+
+    const removedPieceKey = `${x},${y},${z}`;
+
+    const possibleMoves = new Set();
+
+    for (const key in board){
+        const [x, y, z] = board[key].coord;
+
+        if(key === removedPieceKey) continue;
+
+        for (const [dx, dy, dz] of directions){
+
+            const newX = x + dx;
+            const newY = y + dy;
+            const newZ = z + dz;
+
+            const neighbourKey = `${newX},${newY},${newZ}`;
+
+            if(neighbourKey in board)continue;
+
+            let neighbourCount = 0;
+
+            for (const [dx2, dy2, dz2] of directions) {
+                const checkKey = `${newX + dx2},${newY + dy2},${newZ + dz2}`;
+
+                if(checkKey in board && checkKey !== removedPieceKey){
+                    neighbourCount++;
+                }
+            }
+            
+            if(neighbourCount === 2){
+                possibleMoves.add(neighbourKey);
+            }
+        }
+    }
+
+    return Array.from(possibleMoves);
+}
